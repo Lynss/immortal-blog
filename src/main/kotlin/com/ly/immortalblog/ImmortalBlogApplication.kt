84a58b0886ce44f4f6bb375c4622e99e5cf47497
ly <ly1169134156@163.com>
2017-10-19 21:35:40 +0800

    Initial commit

---
+++ src/main/kotlin/com/ly/immortalblog/ImmortalBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.ly.immortalblog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ImmortalBlogApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ImmortalBlogApplication::class.java, *args)
+}
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+//定义了一些文件夹的路径
+const ROOT_PATH = path.resolve(__dirname)
+const APP_PATH = path.resolve(ROOT_PATH, 'app')
+const TEM_PATH = path.resolve(APP_PATH, 'templates')
+const BUILD_PATH = path.resolve(ROOT_PATH, 'src/main/resources')
+
+module.exports = {
+    //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+    entry: {
+        app: path.resolve(APP_PATH, 'index.jsx'),
+        vendors: ['jquery', 'moment']
+    },
+    //输出的文件名 合并以后的js会命名为bundle.js
+    output: {
+        path: BUILD_PATH,
+        filename: 'static/js/[name].[hash].js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:5000',
+                secure: false
+            }
+        },
+        port: 8022
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                enforce: "pre",
+                include: APP_PATH,
+                exclude:/node_modules/,
+                loader: 'eslint-loader'
+            },
+            {
+                test: /\.s?css$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=40000'
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                include: APP_PATH,
+                query: {
+                    presets: ['es2015','react']
+                }
+
+            }
+        ]
+    },
+    //添加我们的插件 会自动生成一个html文件
+    plugins: [
+        //这个使用uglifyJs压缩你的js代码
+        // new webpack.optimize.UglifyJsPlugin({
+        //     minimize: true
+        // }),
+        //把入口文件里面的数组打包成verdors.js
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors',
+            filename: 'static/vendors/vendors.js'
+        }),
+        new HtmlwebpackPlugin({
+            title: 'Hello World app',
+            template: path.resolve(TEM_PATH, 'index.html'),
+            filename: 'templates/index.html',
+            //chunks这个参数告诉插件要引用entry里面的哪几个入口
+            chunks: ['app', 'vendors'],
+            //要把script插入到标签里
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
+    ]
+};
